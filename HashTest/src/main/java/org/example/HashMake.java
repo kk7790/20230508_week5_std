3f76f9c6337984d8279b18a5c719daa791e49e3a
kk7790 <chorang_2307@naver.com>
2023-05-08 21:01:46 +0900

    DataStructure_Update_0508

--- HashTest/src/main/java/org/example/HashMake.java
+++ HashTest/src/main/java/org/example/HashMake.java
@@ -3,43 +3,47 @@ package org.example;
 import java.util.ArrayList;
 
 public class HashMake {
-    ArrayList<Object>[] hashTable = new ArrayList[100];
+    ArrayList<String> hashTable = new ArrayList<String>(10);
 
     //해시 값 만드는 부분..
     public int hashMake(int key){
-        int keyCul = key%8;
-        return keyCul;
+        return key%10;
     }
 
-    public void saveData(int key, Object inputData){
+    public void saveData(int key, String inputData){
         int hashAddress = hashMake(key);
         int nextHash = 0;
-        hashTable.add(inputData);
-//        if(hashTable[hashAddress+nextHash] == null){
-//            hashTable[hashAddress+nextHash] = inputData;
-//        }
-//        else{
-//            for(int success = 0; success<1;){
-//                if(hashTable[hashAddress+nextHash] != null){
-//                    hashTable[hashAddress+nextHash] = inputData;
-//                    nextHash = 0;
-//                    success ++;
-//                }
-//                else{
-//                    nextHash += 1;
-//                }
-//            }
-//        }
+        int inputPoint = hashAddress+nextHash;
+        System.out.println("출력" + hashTable.get(0)); // get 에서 에러발생..
+        String aaa = (hashTable.get(hashAddress + nextHash));
+        //해시값 중복을 피하기 위한
+        System.out.println("출력" + aaa);
+        if(hashTable.get(hashAddress + nextHash) == null){
+            hashTable.add(inputPoint,inputData);
+        }
+        else{
+            for(int success = 0; success<1;){
+                if(hashTable.get(hashAddress + nextHash) != null){
+                    hashTable.add(inputPoint,inputData);
+                    nextHash = 0;
+                    success ++;
+                }
+                else{
+                    nextHash += 1;
+                    inputPoint = hashAddress+nextHash;
+                }
+            }
+        }
 
     }
     public Object readData(int key){
         int hashAddress = hashMake(key);
-        return hashTable[hashAddress];
+        return hashTable.get(hashAddress);
     }
 
     public void deleteData(int key){
         int hashAddress = hashMake(key);
-
+        hashTable.set(hashAddress, null);
     }
 
 }
--- HashTest/src/test/java/org/example/HashMakeTest.java
+++ HashTest/src/test/java/org/example/HashMakeTest.java
@@ -8,35 +8,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HashMakeTest {
-    private long id = (long)1L;
-    private String name = "hutao";
-    private String number = "01012345678";
 
-    public boolean HashMake(long id, String name, String number){
-        this.id = id;
-        this.name = name;
-        this.number = number;
-        return false;
+    @Test
+    void hashAddTest() {
+        HashMake hashmake = new HashMake();
+        hashmake.saveData(1,"lee");
+        String result = (String) hashmake.readData(1);
+        System.out.println("결과 : " + result);
+
+
     }
 
-    public int hashCode(){
-        int hash = 7;
-        hash = 31 * hash + (int)id;
-        hash = 31 * hash + (name == null ? 0 : name.hashCode());
-        hash = 31 * hash + (number == null ? 0 : number.hashCode());
-        System.out.println("hashCode() called - Computed hash : "+ hash);
-        return hash;
-    }
-
-//    @Test
-//    @Override
-//    public boolean equals(Object o){
-//        if (this == o) return true;
-//        if(o == null || getClass() != o.getClass()) return false;
-//        HashMake user = (HashMake) o;
-//        return id == user.id && Objects.equals(name, user.name) &&
-//                Objects.equals(number, user.number);
-//    }
 
 
 }
